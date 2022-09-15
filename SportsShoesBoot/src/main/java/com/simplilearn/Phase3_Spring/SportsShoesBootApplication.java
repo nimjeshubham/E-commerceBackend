378815e19a532477f465ab2bafa17dfe9661ce2c
nimjeshubham <51957013+nimjeshubham@users.noreply.github.com>
2022-09-15 15:41:42 +0530

    Add files via upload

---
+++ SportsShoesBoot/src/main/java/com/simplilearn/Phase3_Spring/SportsShoesBootApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Phase3_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsShoesBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsShoesBootApplication.class, args);
+	}
+
+}
